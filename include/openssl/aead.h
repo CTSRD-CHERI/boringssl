e49b299e18c92b921c1e6da6d863d50cd70f5f63
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-02-22 17:13:36 +0000

    [cheriabi] Enforce alignment to max_align_t.
    A max_alignment filed of typ max_align_t has been added to the union
    evp_aead_ctx_st_state to enforce the alignment of the union to
    alignof(max_align_t). Alternatively an aligned_union type could be
    defined.

--- include/openssl/aead.h
+++ include/openssl/aead.h
@@ -220,7 +220,12 @@ OPENSSL_EXPORT size_t EVP_AEAD_max_tag_len(const EVP_AEAD *aead);
 // AEAD operations.
 
 union evp_aead_ctx_st_state {
+#if defined(__CHERI_PURE_CAPABILITY__)
+  uint8_t opaque[800];
+  max_align_t max_alignment;
+#else // defined(__CHERI_PURE_CAPABILITY__)
   uint8_t opaque[560];
+#endif // defined(__CHERI_PURE_CAPABILITY__)
   uint64_t alignment;
 };
 
